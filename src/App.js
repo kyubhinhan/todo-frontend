c4be3420e99af682bbef420eb57ee9402b90d31c
kyubhinhan <kyubhinhan@gmail.com>
2024-05-11 13:14:39 +0900

    url을 쿠버네티스 서비스에 맞게 수정함

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
-  const url = 'http://127.0.0.1:3001/todos';
+  const url = 'http://192.168.49.2:3001/todos';
 
   useEffect(() => {
     fetchTasks();
